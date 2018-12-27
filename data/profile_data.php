e21dc09571271229a4dcf46853ab6f7933bcaa8b
KateWallet <katewallet95@gmail.com>
2018-12-27 23:31:36 +0300

    Initial commit

---
+++ data/profile_data.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'about' => [
+        'name' => 'Екатерина Кошелькова',
+        'photo' => '',
+        'email' => 'katewallet95@gmail.com',
+        'phone' => '+7 960 248 36 38',
+        'site' => 'katewallet-art.ru',
+        'post' => 'Front-end web developer'
+    ],
+    'education' => [
+        [
+            'title' => 'Санкт-Петербургский государственный университет',
+            'specialty' => 'Бакалавр политических наук',
+            'yearStart' => '2013',
+            'yearEnd' => '2017'
+        ],
+        [
+            'title' => 'Всероссийский государственный университет юстиции',
+            'specialty' => 'Бакалавр юриспруденции',
+            'yearStart' => '2015',
+            'yearEnd' => '2019'
+        ]
+    ],
+    'languages' => [
+        [
+            'title' => 'Русский',
+            'level' => 'Свободный'
+
+        ],
+        [
+            'title' => 'English',
+            'level' => 'Professional'
+        ]
+
+    ],
+    'interests' => [
+        'Веб-разработка',
+        'Написание научных работ',
+        'Йога'
+    ]
+];
